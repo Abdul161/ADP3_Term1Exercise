886b54464636e16d6d330852bfdcf321338b4392
Shuaib1234 <80948707+Shuaib1234@users.noreply.github.com>
2022-02-06 22:17:48 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/Calculator.java
@@ -0,0 +1,16 @@
+package za.ac.cput;
+
+//Student Name - Shuaib Allie
+// Student Number - 217148867
+
+public class Calculator {
+    public int add(int number1, int number2) {
+        int finalresult = number1 + number2;
+        return finalresult;
+    }
+
+    public int subtract(int number1, int number2) {
+        int finalresult = number1 - number2;
+        return finalresult;
+    }
+}
