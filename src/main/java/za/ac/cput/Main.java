2373ff004378d9e539801488ea126546f9350fed
Abdul161 <81579420+Abdul161@users.noreply.github.com>
2022-02-06 21:47:35 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/Main.java
@@ -0,0 +1,18 @@
+package za.ac.cput;
+
+public class Main {
+    public class ShapeRun {
+
+        public static void main(String[] args) {
+            Prisms testingObject = new Prisms();
+            Rectangular testingObject2 = new Rectangular();
+            Triangle testingObject3 = new Triangle();
+
+
+            System.out.println(testingObject.toString());
+            System.out.println(testingObject2.toString());
+            System.out.println(testingObject3.toString());
+
+        }
+
+    }}
